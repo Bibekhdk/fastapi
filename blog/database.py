9ad9802a60ec4a64e5c5d7d3f714dc3ddc2869c2
Bibek Hadkhale <abcbibek246@gmail.com>
2025-07-30 12:09:37 +0545

    comkmit

--- blog/database.py
+++ blog/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # Correct SQLite URL format for absolute path
-SQLALCHEMY_DATABASE_URL = 'sqlite:////home/bibek/Desktop/fastapi/blog.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:////home/bibek/Desktop/fastapi/blog.db'
+
+# Default to 'blog.db' in the current directory if the environment variable is not set
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/blog.db")
+
 
 # Set up the engine, session, and Base
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
